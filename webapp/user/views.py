2f0ef7808232b038bcb06bf99827766368b64c2e
unknown <kseniya-bagnyuk@mail.ru>
2020-01-17 00:49:31 +0700

    DB Migrations & user registration added

--- webapp/user/views.py
+++ webapp/user/views.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_login import current_user, login_user, logout_user
 from flask import Blueprint
 
-from webapp.user.forms import LoginForm
+from webapp import db
+from webapp.user.forms import LoginForm, RegistrationForm
 from webapp.user.models import User 
 
 blueprint = Blueprint('user', __name__, url_prefix='/users')
@@ -10,7 +11,7 @@ blueprint = Blueprint('user', __name__, url_prefix='/users')
 @blueprint.route('/login')
 def login():
 	if current_user.is_authenticated:
-		return redirect(url_for('index'))
+		return redirect(url_for('news.index'))
 	title = "Авторизация"
 	login_form = LoginForm()
 	return render_template('user/login.html', page_title=title, form=login_form)
@@ -33,3 +34,25 @@ def logout():
 	logout_user()
 	flash('Вы успешно разлогинились')
 	return redirect(url_for('news.index'))
+
+@blueprint.route("/register")
+def register():
+	if current_user.is_authenticated:
+		return redirect(url_for('news.index'))
+	title = "Регистрация"
+	form = RegistrationForm()
+	return render_template('user/registration.html', page_title=title, form=form)
+
+@blueprint.route("/process-reg", methods=["POST"])
+def process_reg():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		new_user = User(username=form.username.data, email=form.email.data, role="user")
+		new_user.set_password(form.password.data)
+		db.session.add(new_user)
+		db.session.commit()
+		flash("Вы успешно зарегистрированы")
+		return redirect(url_for("user.login"))
+	flash('Пожалуйста, исправьте ошибки в форме')
+	return redirect(url_for('user.register'))
+
